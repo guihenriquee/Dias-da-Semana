f9bcfc3e81de93d1ba246ec2050c46bb993e66a0
Guilherme Henrique <guilhermehenriquemraissilva@gmail.com>
2025-01-10 11:30:12 -0300

    Add files via upload

---
+++ src/DiasDaSemana.java
@@ -0,0 +1,31 @@
+public class DiasDaSemana {
+    public static void main(String[] args) throws Exception {
+        int dia = 6;
+        switch (dia) {
+            case 1:
+            System.out.println("Segunda-Feira");
+            break;
+            case 2: 
+            System.out.println("Terça-Feira");
+            break;
+            case 3: 
+            System.out.println("Quarta-Feira");
+            break;
+            case 4: 
+            System.out.println("Quinta-Feira");
+            break;
+            case 5: 
+            System.out.println("Sexta-Feira");
+            break;
+            case 6: 
+            System.out.println("Sabado");
+            break;
+            case 7: 
+            System.out.println("Domingo");
+            break;
+            default:
+            System.out.println("Dia invalido");
+            break;
+        }
+    }
+}
